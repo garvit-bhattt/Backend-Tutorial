98eefabc739f12f43294cdbb400ff895afd5731e
GARVIT <garvitbhattpc@gmail.com>
2025-12-10 09:58:56 +0530

    Update: Write data in db.json using writeFileSync

--- Day-3/fs.js
+++ Day-3/fs.js
@@ -6,3 +6,9 @@ const fs = require('fs');
 const data = JSON.parse(fs.readFileSync("./db.json","UTF-8")); //We need to specify encoding in reading 
 console.log(data);
 console.log(data.todos)
+
+
+// Writing a file using writeFileSync Function
+const newTask= {id:4,task:"Learn express",status:"ongoing"}
+data.todos.push(newTask)
+fs.writeFileSync("./db.json",JSON.stringify(data)) // We used stringify because wrtie file sync requires string 
