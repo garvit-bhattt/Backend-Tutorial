e94989934773247d6e93d06cacb8f82d9df7d4b8
GARVIT <garvitbhattpc@gmail.com>
2025-12-10 09:43:50 +0530

    add: read ile using fs module

--- Day-3/fs.js
+++ Day-3/fs.js
@@ -1,1 +1,8 @@
 //fs module is used to read file , it come with node 
+const fs = require('fs');
+
+
+//Reading a file using readFileSync Function
+const data = JSON.parse(fs.readFileSync("./db.json","UTF-8")); //We need to specify encoding in reading 
+console.log(data);
+console.log(data.todos)
