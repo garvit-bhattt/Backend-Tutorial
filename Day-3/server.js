d6b540b4e182c0d331603bb7c3a319a6113511d8
GARVIT <garvitbhattpc@gmail.com>
2025-12-10 10:44:23 +0530

    Add fethcing task get method route

--- Day-3/server.js
+++ Day-3/server.js
@@ -1,10 +1,23 @@
 const express= require('express');
 const app = express();
+const fs = require('fs');
 
 app.get("/health",(req,res)=>{
     res.json({message:"Server is up and running"})
 })
 
+//GET method
+app.get("/tasks",(req,res)=>{
+    const data = JSON.parse(fs.readFileSync("./db.json","UTF-8"));
+    const todos = data.todos
+    res.json({
+        message:"Tasks fethed",
+        Response:todos
+    })
+
+
+
+})
 
 
 
