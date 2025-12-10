a246437ef24306856cb183ac886c9dc087cb49ef
GARVIT <garvitbhattpc@gmail.com>
2025-12-10 14:50:50 +0530

    Add post route

--- Day-3/server.js
+++ Day-3/server.js
@@ -1,27 +1,35 @@
-const express= require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const { getTasks, setTasks } = require("./functions");
 
-app.get("/health",(req,res)=>{
-    res.json({message:"Server is up and running"})
-})
+app.use(express.json()); // This middleware converts JSON → JS object ( Required for POST / PUT / PATCH requests sending JSON )
+
+app.get("/health", (req, res) => {
+  res.json({ message: "Server is up and running" });
+});
 
 //GET method
-app.get("/tasks",(req,res)=>{
-    const data = JSON.parse(fs.readFileSync("./db.json","UTF-8"));
-    const todos = data.todos
-    res.json({
-        message:"Tasks fethed",
-        Response:todos
-    })
+app.get("/tasks", (req, res) => {
+    const data = getTasks();
+    res.json(data);
+});
+//POST method
+app.post("/tasks", (req, res) => {
+  const body = req.body;
+  const data= getTasks();
+  //setting id by checking the value of last id
+  const newId= data.todos[data.todos.length - 1].id +1;
+  const newTask={id:newId,...body};
+  //pushing the code into data
+  data.todos.push(newTask);
+  //saving the data
+  setTasks(data);
 
 
+});
 
-})
+const PORT = 4800;
+app.listen(PORT, () => {
+  console.log(`Server started running on port ${PORT}`);
+});
 
-
-
-const PORT= 4800;
-app.listen(PORT,()=>{
-    console.log(`Server started running on port ${PORT}`)
-})
