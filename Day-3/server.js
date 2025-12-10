a1e378192f7d104875fd2c0e9409c8c127e2df3a
GARVIT <garvitbhattpc@gmail.com>
2025-12-11 02:55:05 +0530

    Add POST /tasks route with auto-increment ID and file persistence

--- Day-3/server.js
+++ Day-3/server.js
@@ -18,12 +18,18 @@ app.post("/tasks", (req, res) => {
   const body = req.body;
   const data= getTasks();
   //setting id by checking the value of last id
-  const newId= data.todos[data.todos.length - 1].id +1;
+  const newId= Number(data.todos[data.todos.length - 1].id) +1;
   const newTask={id:newId,...body};
   //pushing the code into data
   data.todos.push(newTask);
   //saving the data
   setTasks(data);
+
+  res.status(201).json({
+  message: "Task added successfully",
+  task: newTask
+});
+
 
 
 });
